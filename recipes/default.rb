319e98b6ae0cf07c1b54464a5b5901ace0be35a8
Stephane CRIVISIER <stephane@crivisier.fr>
2014-05-11 11:17:38 +0200

    Configurable apt_preference priorities (v1.1.0)

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,9 +1,9 @@
 if node['lsb']['codename'].to_s.empty?
-    Chef::Application.fatal!('The lsb-release package is required.')
+  Chef::Application.fatal!('The lsb-release package is required.')
 end
 
 unless ['squeeze', 'wheezy'].include?(node['lsb']['codename'])
-    Chef::Application.fatal!('A stable Debian system in version >= 6.0 is required.')
+  Chef::Application.fatal!('A stable Debian system in version >= 6.0 is required.')
 end
 
 apt_repository 'dotdeb' do
@@ -19,5 +19,13 @@ end
 apt_preference 'dotdeb' do
   glob         '*'
   pin          'origin packages.dotdeb.org'
-  pin_priority '700'
+  pin_priority node['dotdeb_repo']['apt_preference']['default']
+end
+
+node['dotdeb_repo']['apt_preference']['customs'].each do |apt_preference|
+  apt_preference "dotdeb-#{apt_preference["glob"]}" do
+    glob         apt_preference['glob']
+    pin          'origin packages.dotdeb.org'
+    pin_priority apt_preference['priority']
+  end
 end
